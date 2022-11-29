411ff93122232691e978aefe2289c18ba3d27fac
flux <25628292+fluxionary@users.noreply.github.com>
2022-11-29 11:55:22 -0800

    fmod and normalization

--- init.lua
+++ init.lua
@@ -1,34 +1,6 @@
-local modname = minetest.get_current_modname()
-local modpath = minetest.get_modpath(modname)
-local S = minetest.get_translator(modname)
+futil.check_version({ year = 2022, month = 10, day = 24 })
 
-assert(
-	type(futil.version) == "number" and futil.version >= os.time({ year = 2022, month = 10, day = 24 }),
-	"please update futil"
-)
-
-inventory_ores = {
-	author = "flux",
-	license = "AGPL_v3",
-	version = os.time({ year = 2022, month = 10, day = 12 }),
-	fork = "flux",
-
-	modname = modname,
-	modpath = modpath,
-	S = S,
-
-	has = {
-		unified_inventory = minetest.get_modpath("unified_inventory"),
-	},
-
-	log = function(level, messagefmt, ...)
-		return minetest.log(level, ("[%s] %s"):format(modname, messagefmt:format(...)))
-	end,
-
-	dofile = function(...)
-		return dofile(table.concat({ modpath, ... }, DIR_DELIM) .. ".lua")
-	end,
-}
+inventory_ores = fmod.create()
 
 inventory_ores.dofile("api")
 
